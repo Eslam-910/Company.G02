945e952411138dc972c5af07a9adcf67b90ddee0
esl3 <esl3@ESL3>
2025-07-06 17:37:14 +0300

    Create Create (Action)

--- Company.G02.PL/Controllers/DepartmentController.cs
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -1,5 +1,7 @@
 ﻿using Company.G02.BLL.Interfaces;
 using Company.G02.BLL.Repositories;
+using Company.G02.DAL.Models;
+using Company.G02.PL.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.G02.PL.Controllers
@@ -19,5 +21,31 @@ namespace Company.G02.PL.Controllers
             var department= _departmenRepository.GetAll();
             return View(department);
         }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if (ModelState.IsValid)//Server Side Validation
+            {
+                var department = new Department()
+                {
+                    code = model.Code,
+                    Name = model.Name,
+                    CreateAt = model.CreateAt
+                };
+               var count= _departmenRepository.Add(department);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+            return View(model);
+        }
+
     }
 }
---
+++ Company.G02.PL/Dtos/CreateDepartmentDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.G02.PL.Dtos
+{
+    public class CreateDepartmentDto
+    {
+        [Required(ErrorMessage ="The Code is Reqiured ")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "The Name is Reqiured ")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "The CreatAt is Reqiured ")]
+        public DateTime CreateAt { get; set; }
+
+    }
+}
